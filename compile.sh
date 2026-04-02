8f63fffffff131a5d125dab9f2c6bc6bacf7a5d9
Camleaf <bdaace09@gmail.com>
2026-04-02 14:11:59 -0400

    small stuff

--- compile.sh
+++ compile.sh
@@ -1,4 +1,9 @@
+if [ -d "./bin" ]; then
+    rm -rf ./bin/*  
+fi
 cd build || exit
 rm -rf ./*
 cmake ..
 make
+cd ..
+chmod +x ./bin/cam-cli
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "iostream"
 
 int main (int argc, char *argv[]) {
-
-    std::cout << "Hello";
+    
+    std::cout << "hello" << std::endl;
     return 0;
 }
