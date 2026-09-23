31075bed379d1e0a12395c60ed3d83474118de69
Camleaf <bdaace09@gmail.com>
2026-09-23 13:03:48 -0400

    some more working

--- cam-pw-server/db_operations.py
+++ cam-pw-server/db_operations.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Request, Response
 from .auth import db, PasswordSubmission, checkPassword
-from typing import Any
+from typing import Any, final
 from pydantic import BaseModel
 from google.cloud import firestore_v1
 import json
@@ -36,3 +36,37 @@ async def retrieveStored(passwordSubmission:PasswordSubmission):
 
 
 
+
+MERGE,PRUNE = range(2)
+CURRENT,INCOMING = range(2)
+
+class ContentBox(BaseModel):
+    content:dict[str,dict[str,str]]
+    mode:int
+    priority:int
+
+
+@PWManager.post("/update")
+async def retrieveStored(passwordSubmission:PasswordSubmission, data:ContentBox):
+
+
+    if not checkPassword(passwordSubmission.password):
+        return HTTPException(401, detail=f"Access Denied")
+    
+    loaded_data:dict[str,dict[str,str]] = data.content
+    try:
+        collectionRef = db.collection('pwds')
+
+        ''' 
+        batch = db.batch()
+        batch.set(docRef,req['games'])
+        batch.set(docRef2,req['teams'])
+        batch.set(docRef3, {'scoreboard':req['enabled']})
+        batch.commit()
+        '''
+    except KeyError as e:
+        return HTTPException(422, detail="Missing fields")
+    except Exception as e:
+        return HTTPException(500, detail=f"Internal Server Error")
+    
+    return Response(status_code=200,content="Success",headers={'content-type':'text/plain'})
